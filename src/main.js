56a3182c2a2fd65e06fa2d15ccdf7ff3c7ed7522
Martin Lavoie <broemartino@gmail.com>
2016-08-27 10:37:25 -0400

    Debug basic stuff

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
-var worldGen = {};
+var worldGen = {}
+
+var debug = true;
+var toggleDebug;
 
 function preload() {
     game.load.script("src/world", null, function() {
@@ -11,7 +14,29 @@ function preload() {
 
 function create() {
     worldGen.generate(game);
+    toggleDebug = game.input.keyboard.addKey(Phaser.Keyboard.P);
 }
 
 function update() {
+    if(toggleDebug.isDown) {
+        if(debug)
+        {
+            game.debug.reset();
+        }
+        debug = !debug;
+    }
+}
+
+var origin = new Phaser.Circle(0, 0, 25);
+function render() {
+    if(debug){
+        var x = 5;
+        var y = 0;
+        var yi = 32;
+
+        // Camera
+        game.debug.cameraInfo(game.camera, x, y += yi);
+        game.debug.geom(origin,'rgba(266,0,0,1)');
+
+    }
 }
